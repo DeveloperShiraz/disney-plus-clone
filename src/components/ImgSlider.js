5a3e3a7281b849feecc311cd296e45e7969c5cb5
TrendsetterShiraz <lightningboy78@gmail.com>
2022-08-10 21:10:43 -0500

    Alpha v0.1

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -70,6 +70,7 @@ img {
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
   rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   transition-duration: 300ms;
+  overflow: hidden;
 
   &:hover{
     border: 4px solid rgba(249, 249, 249, 0.8);
