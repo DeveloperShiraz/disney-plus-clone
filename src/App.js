82991a066dd570dc1f2d646b79c98f97b2673632
TrendsetterShiraz <lightningboy78@gmail.com>
2022-08-10 21:32:43 -0500

    Alpha v0.2

--- src/App.js
+++ src/App.js
@@ -4,17 +4,41 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home'
 import Detail from './components/Detail'
-import {BrowserRouter as Router, Routes as Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header/>
-        <Home/>
-      </Router>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Header/>} path="/"/>
+        </Routes>
+        <Routes>
+          <Route element={<Home/>} path="/"/>
+        </Routes>
+        <Routes>
+          <Route element={<Header/>} path="/detail"/>
+        </Routes>
+        <Routes>
+          <Route element={<Detail/>} path="/detail"/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+/*
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+*/
